1c935bbe9357c82392e424a4f2ac235fbd7c7073
AuraFly <JordanJCo@gmail.com>
2022-05-09 20:13:54 -0600

    uploading schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,34 @@
---WIP
+DROP DATABASE IF EXISTS org_db;
+CREATE DATABASE org_db;
+
+USE org_db;
+
+CREATE TABLE Department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE jobprofile (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE Employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    jobprofile_id INT,
+    manager_id INT,
+    FOREIGN KEY (jobprofile_id)
+    REFERENCES jobprofile(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
+
+);
